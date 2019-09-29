271dfa3aca14be9c61573ad0675b87d40637b0ae
bondarenkom <m.bondarenko@gmail.com>
2019-09-29 19:37:08 +0100

    New files

---
+++ sw.js
@@ -0,0 +1,27 @@
+var staticUrls = [
+    './',
+    './index.html',
+    './styles.css',
+    './image.gif'
+]
+
+self.addEventListener('install', function(event){
+    console.log('[Service Worker] Install');
+    event.waitUntil(
+        caches.open('pwaTutorialCache')
+            .then(function(cache){
+                console.log('[Service Worker] Caching Static Files');
+                return cache.addAll(staticUrls);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event){
+    console.log('[Service Worker] Fetch');
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response){
+                return response || fetch(event.request);
+             })
+    );
+});
